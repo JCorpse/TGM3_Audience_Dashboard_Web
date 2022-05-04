55685da7915a496e7f840517b8bca27f8c407604
JCorpse <24427007+JCorpse@users.noreply.github.com>
2022-05-04 18:35:31 +0800

    first vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,58 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id='app'>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+const API_BASE_URL = "https://tgm3-audience-dashboard.herokuapp.com/api/";
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      Trains: [],
+      testdata: [99, 71, 78, 25, 36, 92]
+    }
+  },
+  methods: {
+    async Request(url) {
+      const data = await (await (fetch(url)
+          .then((res) => res.json())
+          .catch((err) => {
+            console.log("Error ", err)
+          })));
+      return data;
+    },
+    async getTrainData() {
+      const url = "Trains";
+      this.Trains = await (this.Request(`${API_BASE_URL}${url}`));
+      console.log(this.Trains)
+    },
+    draw() {
+
+    }
+  },
+  mounted() {
+    this.getTrainData();
+    this.draw();
   }
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /*text-align: center;*/
   color: #2c3e50;
   margin-top: 60px;
+}
+
+svg {
+  margin: 25px;
+  fill: none;
+  stroke: #76BF8A;
+  stroke-width: 3px;
 }
 </style>
