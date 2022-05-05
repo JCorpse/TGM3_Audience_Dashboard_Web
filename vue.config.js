c1b2c0e26e6b0c0a713481dd618254e9c42e1366
JCorpse <24427007+JCorpse@users.noreply.github.com>
2022-05-05 15:21:54 +0800

    test CICD

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const {defineConfig} = require('@vue/cli-service')
 
 module.exports = defineConfig({
     transpileDependencies: true,
+    publicPath:'/TGM3_Audience_Dashboard_Web/',
     pages:{
         index:{
             entry:'src/main.js',
