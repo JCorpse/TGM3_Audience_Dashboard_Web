dd1e8df495605fecbc05e155df1eaa9b6c64c2ef
JCorpse <24427007+JCorpse@users.noreply.github.com>
2022-05-05 18:30:47 +0800

    add element-plus

--- src/Chat.vue
+++ src/Chat.vue
@@ -1,11 +1,31 @@
 <template>
   <div id="app">
+    <el-main>
     <iframe id="twitch-chat-embed" src="" height="500" width="350"> </iframe>
+
+    <el-form :inline="true" :model="formInline" >
+      <el-form-item label="Token">
+        <el-input v-model="this.Token" />
+      </el-form-item>
+      <el-form-item label="Msg">
+        <el-input v-model="this.Msg" />
+      </el-form-item>
+      <el-form-item>
+        <el-button type="button" @click="postData(this.API_URL)">Query</el-button>
+      </el-form-item>
+    </el-form>
+    </el-main>
+
     <form action="/bot/send" id="sendform" method="post" target="hide_iframe">
       <input type="password" name="token" id="token" />
       <input type="text" name="msg" id="msg" />
-      <input type="button" value="submit" name="submit" id="submit"
-      onclick=postData()>
+      <input
+        type="button"
+        value="submit"
+        name="submit"
+        id="submit"
+        onclick="postData()"
+      />
     </form>
   </div>
 </template>
@@ -14,6 +34,13 @@
 
 export default {
   name: "chat-page",
+  data() {
+    return {
+      API_URL:'https://tgm3-audience-dashboard.herokuapp.com/bot/send',
+      Token:'',
+      Msg:''
+    };
+  },
   methods: {
     async setchat() {
       document.getElementById("twitch-chat-embed").src =
@@ -22,9 +49,10 @@ export default {
     },
     async postData(url) {
       return fetch(url, {
-        body: new URLSearchParams(
-          new FormData(document.getElementById("sendform"))
-        ).toString(),
+        body: {
+          'token':this.token,
+          'msg':this.Msg
+        },
         headers: {
           "user-agent":
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36",
--- src/js/chat.js
+++ src/js/chat.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import App from '../Chat.vue'
 
-createApp(App).mount('#app')
+createApp(App).use(ElementPlus).mount('#app')
